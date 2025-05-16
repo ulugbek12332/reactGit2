e5e69080b465a4a6bf028a4d4ec005fdf24f1d79
ulugbek12332 <ur710132@gmail.com>
2025-05-16 18:56:49 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex-col flex items-center inset-12'>
+      <h1 className=''>Dive into Delights Of Delectable <span className='bg-green-500'>Food</span></h1>
+      <p>Where Each Plate Weaves a Story of Culinary
+         Mastery and Passionate Craftsmanship</p>
+      <button className='bg-emerald-500 rounded-sm'>Order Now</button>
+      <div className='top-1/4'>
+        <h1>Popular Categories</h1>
+        <div className='flex gap-7'>
+
+        <div className='bg-amber-200 rounded-sm'><h1>Main Dish</h1><p>(86 dishes)</p></div>
+        <div className='bg-amber-200 rounded-sm'><h1>Break Fast</h1><p>(12 break fast)</p></div>
+        <div className='bg-amber-200 rounded-sm'><h1>Dessert</h1><p>(48 dessert)</p></div>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
